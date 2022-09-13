cece0a35331c0dde6cda2d9e771c0090a519fd66
Tommie1236 <90513578+Tommie1236@users.noreply.github.com>
2022-09-13 17:39:05 +0200

    updated some more code (find img location)

--- new-script.js
+++ new-script.js
@@ -1,23 +1,34 @@
-function createCard(gridDiv, img, title, url) {
+function createCard(gridDiv, cardData) {
     const cardDiv = document.createElement('div');
     cardDiv.classList.add('card');
-    cardDiv.setAttribute('onclick', `location.href="${url}"`);
+    cardDiv.setAttribute('onclick', `location.href="${cardData.url}"`);
     const cardImg = document.createElement('img');
     cardImg.classList.add('card-img');
-    cardImg.setAttribute('src', img);
+    cardImg.setAttribute('src', cardData.img);
     const cardTitle = document.createElement('h4');
     cardTitle.classList.add('card-title');
-    cardTitle.innerHTML = title;
+    cardTitle.innerHTML = cardData.title;
 
     cardDiv.appendChild(cardImg);
     cardDiv.appendChild(cardTitle);
     gridDiv.appendChild(cardDiv);
 };
 
+/*const cardData = {
+    title: 'title',
+    url: 'url',
+    img: 'imgStr'
+}*/
+
 $.getJSON('https://www.reddit.com/r/persoonlijkebonus/.json', function(data) {
     const posts = data['data']['children'];
     const gridDiv = document.getElementById('gridDiv')
     for (let i = 0; i < posts.lenght; i++) {
-        createCard(gridDiv, )
+        let cardData = {
+            title: posts[i]['data']['title'],
+            url: posts[i]['data']['data'],
+            img: posts[i]['data']['media_metadata'][1] // [p]/[s]/[u]->([1])->[u]
+        }
+        createCard(gridDiv, cardData);
     }
 })
