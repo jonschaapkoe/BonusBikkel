6f7bfabf28fbefaac7d5dad3808fe87b4d2f78e8
Tommie1236 <90513578+Tommie1236@users.noreply.github.com>
2022-09-14 18:11:42 +0200

    updated some code

--- new-script.js
+++ new-script.js
@@ -1,4 +1,4 @@
-function createCard(gridDiv, cardData) {
+function createCard(/*gridDiv, */cardData) {
     const cardDiv = document.createElement('div');
     cardDiv.classList.add('card');
     cardDiv.setAttribute('onclick', `location.href="${cardData.url}"`);
@@ -11,7 +11,7 @@ function createCard(gridDiv, cardData) {
 
     cardDiv.appendChild(cardImg);
     cardDiv.appendChild(cardTitle);
-    gridDiv.appendChild(cardDiv);
+    /*gridDiv.appendChild(cardDiv);*/
 };
 
 /*const cardData = {
@@ -22,14 +22,15 @@ function createCard(gridDiv, cardData) {
 
 $.getJSON('https://www.reddit.com/r/persoonlijkebonus/.json', function(data) {
     const posts = data['data']['children'];
-    const gridDiv = document.getElementById('gridDiv')
+    const gridDiv = document.getElementById('gridDiv');
+    cards = [];
     for (let i = 0; i < posts.lenght; i++) {
         let cardData = {
             title: posts[i]['data']['title'],
             url: posts[i]['data']['data'],
-            img: posts[i]['data']['media_metadata'][1] // [p]/[s]/[u]->([0])->[u]
-        }
-        createCard(gridDiv, cardData);
-    }
+            img: posts[i]['data']['media_metadata'][1] /* [p]/[s]/[u]->([0])->[u] */
+        };
+        cards.apppend(createCard(/*gridDiv, */cardData));
+    };
+    console.log(cards);
 })
-functionsearchBonusBoxes() 
